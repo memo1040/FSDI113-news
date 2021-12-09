521c6ec03cffeba58d8f8be72fad1d861a2fe443
Guillermo Monge <gmt1040@gmail.com>
2021-12-08 20:35:28 -0800

    Assignment 3

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
-from .views import (
-    HomePageView, 
-    SignUpView
-)
+from .views import SignUpView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='index'),
     path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,7 @@
 from django.views.generic.edit import CreateView
-from django.views.generic import TemplateView
 from django.urls import reverse_lazy
-from accounts.models import CustomUser
 from .forms import CustomUserCreationForm
 
-
-
-class HomePageView(TemplateView):
-    template_name = "index.html"
 
 class SignUpView(CreateView):
     template_name = 'registration/signup.html'
